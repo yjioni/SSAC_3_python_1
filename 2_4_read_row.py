7565fcde7e64f747e0f11ad5c1483b78cdf2c55e
yjioni <oing922@gmail.com>
2021-10-03 21:42:04 +0900

    unit2.1.4. row filtering  - add expressions

--- 2_4_read_row.py
+++ 2_4_read_row.py
@@ -29,13 +29,25 @@ print()
 print(sample[condition1])
 print()
 
-condition2 = (sample['성별'] == '남성') & (sample['입국객수'] >= 100000)
+# 가독성을 높이기 위해 줄변경해서 작성하기 `\` 사용
+#  성별이 '남성' 이면서, 입국객수가 10만 이상인 것 출력
+condition2 = (sample['성별'] == '남성') \
+             & (sample['입국객수'] >= 100000)
 print(sample[condition2])
 print()
 
-condition3 = (sample['국적코드'] == 'A01')|(sample['국적코드'] == 'A18')
+# | == or조건, 여러 조건 중 한가지만 만족하면 출력력
+# # 국적코드가 A01 or A18인 것 출력
+condition3 = (sample['국적코드'] == 'A01') \
+             | (sample['국적코드'] == 'A18')
 print(sample[condition3])
 print()
 
+# | == or == isin['조건1', '조건2']
 condition4 = (sample['국적코드'].isin(['A01', 'A18']))
 print(sample[condition4])
+print()
+
+# 조건에 해당하지 않는 것 출력
+condition5 = (sample['국적코드'].isin(['A01', 'A18']))
+print(sample[condition5 == False])
